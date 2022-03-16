cd7f88d08d93427373131431c66deebb424ea156
Aziz Moe <66800426+a2uuz@users.noreply.github.com>
2022-03-16 23:24:05 +0300

    added js file

---
+++ app.js
@@ -0,0 +1,13 @@
+let emailInput = document.getElementById('email');
+let button = document.getElementById('arrow');
+
+
+button.addEventListener('click', ()=>{
+
+})
+
+emailInput.addEventListener('keydown', (e)=>{
+    if(e.key === 'Enter'){
+    
+    }
+})
