147e6f31a3c51a4ac69abe65766e70ec068c29ec
Aziz Moe <66800426+a2uuz@users.noreply.github.com>
2022-03-21 12:29:25 +0300

    all done

--- app.js
+++ app.js
@@ -1,13 +1,26 @@
-let emailInput = document.getElementById('email');
+// let pattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
+
 let button = document.getElementById('arrow');
 
+button.addEventListener('click', () =>{
+let form = document.getElementById('form');
+let email = document.getElementById('email').value;
+let text = document.getElementById('text');
+let pattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
 
-button.addEventListener('click', ()=>{
 
-})
+if (email.match(pattern)){
+  form.classList.add('valid');
+  form.classList.remove('invalid');
+  // text.textContent = `Email adress is valid.`;
+  // text.style.color = '#16f321';
+  
+}
+else{
+  form.classList.remove('valid');
+  form.classList.add('invalid');
+  text.textContent = `Please provide a valid email`;
+  text.style.color = '#ff0000';
+}
 
-emailInput.addEventListener('keydown', (e)=>{
-    if(e.key === 'Enter'){
-    
-    }
 })
